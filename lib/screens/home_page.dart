bd8fe7616f5816976897b4c4a896ab7b7216133f
tetema <tetema@student.wethinkcode.co.za>
2021-12-20 16:05:42 +0200

    Found a way to render SVG icons:)

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,30 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:jot_somethin/main.dart';
-import 'package:switcher_button/switcher_button.dart';
+import 'package:jot_somethin/svg_widgets/rendering_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        scrollDirection: Axis.vertical,
-        children: <Widget>[
-          Container(
-            color: Colors.pink,
-            child: const Center(
-              child: Text('Page 1')
-            )
-          ),
-          Container(
-            color: Colors.cyan
-          ),
-          Container(
-            color: Colors.amber
-          )
-        ],
+      body: Center(
+        child: svgIcon(),
       ),
+    );
+  }
+
+  PageView pageViewTingz() {
+    return PageView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        homeSide(),
+        quotesSide()
+      ],
+    );
+  }
+
+  Widget homeSide() {
+    return PageView(
+      scrollDirection: Axis.vertical,
+      children: <Widget>[
+        Container(
+          color: Colors.black38,
+          child: const Center(
+            child: Text("Today Weather"),
+          )
+        ),
+        Container(
+          color: Colors.blueAccent,
+          child: const Center(
+            child: Text("5 Day Forecast"),
+          )
+        )
+      ],
+    );
+  }
+
+  Widget quotesSide() {
+    return PageView(
+      scrollDirection: Axis.vertical,
+      children: <Widget>[
+        Container(
+          color: Colors.deepPurpleAccent,
+          child: const Center(
+            child: Text("Quote"),
+          )
+        ),
+        Container(
+          color: Colors.amberAccent,
+          child: const Center(
+            child: Text("Quote 2"),
+          )
+        )
+      ],
     );
   }
 }
---
+++ lib/svg_widgets/rendering_svg.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/material.dart';
+
+const String assetName = 'assets/weather-icons/light_mode/snow_1.svg';
+
+Widget svgIcon() {
+  return Center(
+    child: SvgPicture.asset(
+      assetName,
+      semanticsLabel: 'The Things man!',
+      height: 100,
+    )
+  );
+}
